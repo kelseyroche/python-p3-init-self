7d7dab0c72dfd9bf2cb053f6be9c161974e3f73b
Kelsey Roche <roche.kelsey.24@gmail.com>
2024-12-10 09:55:59 -0500

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+my_dog = Dog("Buddy")
+print(my_dog.name)  
+print(my_dog.breed) 
+
+another_dog = Dog("Rex", "German Shepherd")
+print(another_dog.name) 
+print(another_dog.breed) 
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__ (self, name):
+        self.name = name
+
+my_person = Person ("Kelsey")
+print(my_person.name)
